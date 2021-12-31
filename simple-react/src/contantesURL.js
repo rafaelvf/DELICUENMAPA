aae5f65c7d393f83ad77a8bc07a52489fa89fbd9
Rafael Viteri <reviterif@gmail.com>
2021-12-31 10:18:42 -0500

    cambios

---
+++ simple-react/src/contantesURL.js
@@ -0,0 +1,5 @@
+const URLrequests = "https://frozen-lake-57992.herokuapp.com/"
+//const URLrequests = "http://localhost:5000/" 
+
+
+export default URLrequests;
--- simple-react/src/redux/actions.js
+++ simple-react/src/redux/actions.js
@@ -1,10 +1,11 @@
 import { ActionTypes } from "./action-types"
 import axios from "axios";
+import URLrequests from '../contantesURL'
 
 export const GET_ROBOS=()=>{//esta action me trae todos los paises.
     
     return async function (dispatch){
-        const res= await axios.get("http://localhost:5000/customers");
+        const res= await axios.get(`${URLrequests}customers`);
     dispatch({
         type: ActionTypes.GET_ROBOS,
         payload: res.data,
