be6b48b3f3d6c8bdfa3f0d51199909cdf232f1cd
Rafael Viteri <reviterif@gmail.com>
2022-01-03 20:24:24 -0500

    cambio en el popup

--- simple-react/src/components/Markers.js
+++ simple-react/src/components/Markers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Marker } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import {Icon} from 'leaflet';
 
@@ -9,7 +9,11 @@ const Markers = (props)=>{
     console.log(robos,"robos")
     
     const markers=robos && robos.length ? robos.map((place,i)=>(
-        <Marker key={i} position={place.coordenadas} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} />
+        <Marker key={i} position={place.coordenadas} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
+        <Popup>Fecha y hora del robo: {place.fechaHora}<br/>
+        Que robaron: {place.articulo}<br/>Como robaron: {place.tipo}<br/>Que paso (opcional): {place.descripcion}
+        </Popup>
+        </Marker>
     )):null
 
     return markers && markers.length?(
