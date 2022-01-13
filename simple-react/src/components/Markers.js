d6a550f09f57bae2a0dcb2b5de7faaf79fbe9b01
Rafael Viteri <reviterif@gmail.com>
2022-01-13 11:50:24 -0500

    ultimos cambios

--- simple-react/src/components/Markers.js
+++ simple-react/src/components/Markers.js
@@ -11,7 +11,7 @@ const Markers = (props)=>{
     const markers=robos && robos.length ? robos.map((place,i)=>(
         <Marker key={i} position={place.coordenadas} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
         <Popup>Fecha y hora del robo: {place.fechaHora}<br/>
-        Que robaron: {place.articulo}<br/>Como robaron: {place.tipo}<br/>Que paso (opcional): {place.descripcion}
+        Qué robaron: {place.articulo}<br/>Cómo robaron: {place.tipo}<br/>Qué paso (opcional): {place.descripcion}
         </Popup>
         </Marker>
     )):null
